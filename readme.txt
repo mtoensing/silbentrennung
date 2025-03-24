abf633bbd3057dfc1406e041e8038e4c7a913ca3
Marc Toensing <marc.toensing@gmail.com>
2025-03-24 18:58:16 +0100

    1.4

--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: hyphens, text formatting, CSS
 Requires at least: 5.0
 Tested up to: 6.7
 Requires PHP: 7.2
-Stable tag: 1.3
+Stable tag: 1.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
--- silbentrennung.php
+++ silbentrennung.php
@@ -3,7 +3,7 @@
  * Plugin Name: Silbentrennung
  * Plugin URI: http://toensing.com/silbentrennung
  * Description: Adds CSS for text hyphenation across the site.
- * Version: 1.3
+ * Version: 1.4
  * License: GPLv2 or later
  * Author: Marc Tönsing
  * Author URI: https://toensing.com
